315083f7bc9d672f21d103381b1ea9dec9c65f49
HumbertoSubiza <walter.pina1954@gmail.com>
2020-11-25 17:25:25 +0000

    crea stars_1_esp

---
+++ github_rstudio.R
@@ -0,0 +1,4 @@
+usethis::use_git_config(user.name = "HumbertoSubiza", # Seu nome
+                        user.email = "walter.pina1954@gmail.com")
+usethis::browse_github_token()
+usethis::edit_r_environ()
